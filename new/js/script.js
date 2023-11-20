ba6fa978fe3889c82aeef4d5657de36d8cbbf290
alaashawky <104537688+AlaaShawkyy@users.noreply.github.com>
2023-11-20 19:25:28 +0200

    Add files via upload

---
+++ new/js/script.js
@@ -0,0 +1,32 @@
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+
+  }
+  
+  window.addEventListener("scroll", reveal);
+  
+  document.querySelectorAll(".nav-link").forEach((link) => {
+    if (link.href === window.location.href) {
+        link.classList.add("active");
+        link.setAttribute("aria-current", "page");
+    }
+});
+
+const checkbox = document.getElementById("checkbox")
+checkbox.addEventListener("change", () => {
+  document.body.classList.toggle("dark")
+})
+
+
